69f191f36fd9739d9e53b978c7f0d24f3029fa33
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2023-05-16 06:55:04 -0400

    @Overriden the equals() method

--- src/com/ykomarnytskyi2022/freight/Shipment.java
+++ src/com/ykomarnytskyi2022/freight/Shipment.java
@@ -1,10 +1,13 @@
 package com.ykomarnytskyi2022.freight;
 
 import java.util.Map;
+import java.util.Objects;
 
 import com.ykomarnytskyi2022.exel_parser.FieldsTransmitter;
 
+
 class Shipment {
+	private BasicShipmentFields organizationName;
 	private BasicShipmentFields shipmentNumber;
 	private BasicShipmentFields shipmentID;
 	private ShipmentStatus status;
@@ -21,6 +24,7 @@ class Shipment {
 	@SuppressWarnings("unlikely-arg-type")
 	public Shipment(FieldsTransmitter ft) {
 		Map<BasicShipmentFields, String > mapOFields = ft.getMapOfAbsorbedFields();
+		organizationName = BasicShipmentFields.fromString(mapOFields.get(organizationName));
 		shipmentNumber =  BasicShipmentFields.fromString(mapOFields.get(shipmentNumber));
 		shipmentID = BasicShipmentFields.fromString(mapOFields.get(shipmentID));
 		status = ShipmentStatus.fromString(mapOFields.get(status));
@@ -33,5 +37,17 @@ class Shipment {
 		PNLT = BasicShipmentFields.fromString(mapOFields.get(PNLT));
 		DNET = BasicShipmentFields.fromString(mapOFields.get(DNET));
 		DNLT = BasicShipmentFields.fromString(mapOFields.get(DNLT));
+	}
+	
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+	@Override
+	public boolean equals(Object obj) {
+		return obj instanceof Shipment && 
+			Objects.equals(shipmentID, ((Shipment) obj).shipmentID) &&
+			Objects.equals(organizationName, ((Shipment) obj).organizationName);
 	}
 }
