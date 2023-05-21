aab289edd5676f36055a418104ec3250ea2dda6e
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2023-05-21 11:39:49 -0400

    Changed the type of the magority of private feilds from BasicShipmentFields to String (also adapted the constructor method to the aformentioned changes). Two methods were added: getOriginPlaceAndState() & getDestinationPlaceAndState() as well as a bunch of field getters.

--- src/com/ykomarnytskyi2022/freight/Shipment.java
+++ src/com/ykomarnytskyi2022/freight/Shipment.java
@@ -2,41 +2,67 @@ package com.ykomarnytskyi2022.freight;
 
 import java.util.Map;
 import java.util.Objects;
-
 import com.ykomarnytskyi2022.exel_parser.FieldsTransmitter;
 
-
-public class Shipment implements Comparable<Shipment> {
-	private BasicShipmentFields organizationName;
-	private BasicShipmentFields shipmentNumber;
-	private BasicShipmentFields shipmentID;
-	private ShipmentStatus status;
-	private BasicShipmentFields scac;
-	private BasicShipmentFields originCity;
-	private BasicShipmentFields destinationCity;
-	private BasicShipmentFields originState; 
-	private BasicShipmentFields destinationState;  
-	private BasicShipmentFields PNET; 
-	private BasicShipmentFields PNLT; 
-	private BasicShipmentFields DNET; 
-	private BasicShipmentFields DNLT; 
+public class Shipment extends Trackable implements Comparable<Shipment> {
 	
-	@SuppressWarnings("unlikely-arg-type")
+	private String organizationName;
+	@SuppressWarnings("unused")
+	private String shipmentNumber;
+	private String shipmentID;
+	private ShipmentStatus status;
+	@SuppressWarnings("unused")
+	private String scac;
+	private String originCity;
+	private String destinationCity;
+	private String originState; 
+	private String destinationState;  
+	private String PNET; 
+	@SuppressWarnings("unused")
+	private String PNLT; 
+	@SuppressWarnings("unused")
+	private String DNET; 
+	private String DNLT; 
+	
+	public String getOriginCity() {
+		return originCity;
+	}
+
+	public String getDestinationCity() {
+		return destinationCity;
+	}
+
+	public String getOriginState() {
+		return originState;
+	}
+
+	public String getDestinationState() {
+		return destinationState;
+	}
+
 	public Shipment(FieldsTransmitter ft) {
 		Map<BasicShipmentFields, String > mapOFields = ft.getMapOfAbsorbedFields();
-		organizationName = BasicShipmentFields.fromString(mapOFields.get(organizationName));
-		shipmentNumber =  BasicShipmentFields.fromString(mapOFields.get(shipmentNumber));
-		shipmentID = BasicShipmentFields.fromString(mapOFields.get(shipmentID));
-		status = ShipmentStatus.fromString(mapOFields.get(status));
-		scac = BasicShipmentFields.fromString(mapOFields.get(scac));
-		originCity = BasicShipmentFields.fromString(mapOFields.get(originCity));
-		destinationCity = BasicShipmentFields.fromString(mapOFields.get(destinationCity));
-		originState = BasicShipmentFields.fromString(mapOFields.get(originState));
-		destinationState = BasicShipmentFields.fromString(mapOFields.get(destinationState));
-		PNET = BasicShipmentFields.fromString(mapOFields.get(PNET));
-		PNLT = BasicShipmentFields.fromString(mapOFields.get(PNLT));
-		DNET = BasicShipmentFields.fromString(mapOFields.get(DNET));
-		DNLT = BasicShipmentFields.fromString(mapOFields.get(DNLT));
+		try {
+			organizationName = mapOFields.get(BasicShipmentFields.ORGANIZATION_NAME);
+			shipmentNumber =  mapOFields.get(BasicShipmentFields.SHIPMENT_NUMBER);
+			shipmentID = mapOFields.get(BasicShipmentFields.SHIPMENT_ID);
+			status = ShipmentStatus.fromString(mapOFields.get(BasicShipmentFields.STATUS));
+			scac = mapOFields.get(BasicShipmentFields.SCAC_CODE);
+			originCity = this.prettifyLocationName(mapOFields.get(BasicShipmentFields.ORIGIN));
+			destinationCity = this.prettifyLocationName(mapOFields.get(BasicShipmentFields.DESTINATION));
+			originState = mapOFields.get(BasicShipmentFields.ORIGIN_STATE);
+			destinationState = mapOFields.get(BasicShipmentFields.DESTINATION_STATE);
+			PNET = mapOFields.get(BasicShipmentFields.PNET);
+			PNLT = mapOFields.get(BasicShipmentFields.PNLT);
+			DNET = mapOFields.get(BasicShipmentFields.DNET);
+			DNLT = mapOFields.get(BasicShipmentFields.DNLT);			
+		} catch (NullPointerException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+	
+	public ShipmentStatus getStatus() {
+		return status;
 	}
 	
 	@Override
@@ -52,12 +78,23 @@ public class Shipment implements Comparable<Shipment> {
 			Objects.equals(originState, ((Shipment) obj).originState) && 
 			Objects.equals(destinationCity, ((Shipment) obj).destinationCity) && 
 			Objects.equals(destinationState, ((Shipment) obj).destinationState);
-	}
-
+	}	
+	
 	@Override
 	public int compareTo(Shipment o) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
+
+	@Override
+	public String getOriginPlaceAndState() {
+		return originCity + ", " + originState;
+	}
+
+	@Override
+	public String getDestinationPlaceAndState() {
+		return destinationCity + ", " + destinationState;
+	}
+	
 
 }
