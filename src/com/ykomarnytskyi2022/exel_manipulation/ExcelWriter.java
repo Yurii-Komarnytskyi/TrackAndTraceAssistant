ed7a5f6b002af62536da4e7ae38bdf3fea020680
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2023-05-25 05:14:00 -0400

    modified the writeToExcel() to properly work with indexes within IntStream.range added new method: writeToExcelFromMultFiles(List<Set<FieldsTransmitter>> listOfParsedFiles, ExcelWriter writer) added new method: mapFromFieldsTransToShipment(Set<FieldsTransmitter> ftSet)

--- src/com/ykomarnytskyi2022/exel_manipulation/ExcelWriter.java
+++ src/com/ykomarnytskyi2022/exel_manipulation/ExcelWriter.java
@@ -5,11 +5,10 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -19,34 +18,43 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 import com.ykomarnytskyi2022.freight.Shipment;
 
-class ExcelWriter extends PathSharer_BNN  {
+class ExcelWriter extends PathSharer_BNN {
 
-	
-	public <T extends Shipment> void writeToExcel(String pathToCleanFile, String sheetName, List<Shipment> parsedFreight) {
+	private String pathToCleanFile;
+	private String sheetName;
+	private int latestWrittenRowIndex = 0;
+	private int witeShipmentIntoRowNumber = 0;
+
+	public ExcelWriter(String filePath, String sheetName) {
+		pathToCleanFile = filePath;
+		this.sheetName = sheetName;
+	}
+
+	<T extends Shipment> void writeToExcel(List<Shipment> parsedFreight) {
+
 		try {
 			FileInputStream fis = new FileInputStream(pathToCleanFile);
 			Workbook wb = WorkbookFactory.create(fis);
 			Sheet sheet = wb.getSheet(sheetName);
-//			Row currentRow;
-//			Cell currentCell;
-			IntStream.range(0, parsedFreight.size())
-				.forEach(n -> {
-					Row currentRow = sheet.getRow(n);
-					Cell currentCell;
-					System.out.println(currentRow);
-					
-					String[] arr = parsedFreight.get(n).presentFdsToWriter();
-					for(int i = 0; i < arr.length; i++) {
-					currentCell = currentRow.createCell(i);
-					currentCell.setCellValue(arr[i]);				
-				}
+
+			// Runs through ROWS
+			IntStream.range(latestWrittenRowIndex, latestWrittenRowIndex + parsedFreight.size()).forEach(n -> {
+				Row currentRow = sheet.getRow(n);
+				String[] arr = parsedFreight.get(witeShipmentIntoRowNumber).presentFdsToWriter();
+				witeShipmentIntoRowNumber++;
+				
+//				Runs through CELLS 
+				IntStream.range(0, arr.length).forEach(i -> {
+					Cell currentCell = currentRow.createCell(i);
+					currentCell.setCellValue(arr[i]);
 				});
-			
-			
-			
+			});
+
 			FileOutputStream fos = new FileOutputStream(pathToCleanFile);
 			wb.write(fos);
-			
+			latestWrittenRowIndex += parsedFreight.size() + 2;
+			witeShipmentIntoRowNumber = 0;
+
 		} catch (EncryptedDocumentException e) {
 			e.printStackTrace();
 		} catch (FileNotFoundException e) {
@@ -55,21 +63,35 @@ class ExcelWriter extends PathSharer_BNN  {
 			e.printStackTrace();
 		}
 	}
-	
-	public static void main(String[] args) {
-		ExcelWriter ew = new ExcelWriter();
-		ExcelParser parsedExelFile = new ExcelParser(ew.CENTRIA_PATH, ew.SEARCH_RESULTS);
-		List<FieldsTransmitter> t2 = new ArrayList<>(parsedExelFile.parseAllLoads__Bnn(parsedExelFile.CENTRIA_PATH)); 
-		
-		List<Shipment> shList = t2.stream()
-				.map(ft -> new Shipment(ft))
-//				.peek(sh -> System.out.println(sh.getSatusUpd(sh)))
-				.collect(Collectors.toCollection(ArrayList::new));
-		
-		ew.writeToExcel(ew.WRITE_TO, ew.SHEET_NAME, shList);
+
+//	indicateNewMethod in next commit
+	<T extends Shipment> void writeToExcelFromMultFiles(List<Set<FieldsTransmitter>> listOfParsedFiles,
+			ExcelWriter writer) {
+
+		listOfParsedFiles.stream()
+			.map(fieldsTransm -> writer.mapFromFieldsTransToShipment(fieldsTransm))	
+			.forEach(shipm -> {
+				writer.writeToExcel(shipm);
+			});
 	}
 
-	
-	
+	// indicateNewMethod in next commit
+	List<Shipment> mapFromFieldsTransToShipment(Set<FieldsTransmitter> ftSet) {
+		return ftSet.stream()
+				.map(ft -> new Shipment(ft))
+				.collect(Collectors.toCollection(ArrayList::new));
+	}
+
+	public static void main(String[] args) {
+		ExcelWriter write = new ExcelWriter(ExcelWriter.WRITE_TO, ExcelWriter.SHEET_NAME);
+		List<ExcelParser> testList = new ArrayList<>();
+		testList.add(new ExcelParser(write.MHS_PATH, write.SEARCH_RESULTS));
+		testList.add(new ExcelParser(write.CENTRIA_PATH, write.SEARCH_RESULTS));
+		testList.add(new ExcelParser(write.STEEL_PATH, write.SEARCH_RESULTS));
+
+		write.writeToExcelFromMultFiles(ExcelParser.readFromMultipleFiles(testList), write);
+
+
+	}
 
 }
