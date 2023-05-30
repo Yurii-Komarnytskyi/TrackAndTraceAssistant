9b63cc6463c5091a96dcfb4dd95947a96af65c74
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2023-05-30 09:04:20 -0400

    Designed class ReadWriteMiddleman, going to be used as a launcher that accepts ExcelWriter & ExcelParser objs for further combined use.

---
+++ src/com/ykomarnytskyi2022/exel_manipulation/ReadWriteMiddleman.java
@@ -0,0 +1,53 @@
+package com.ykomarnytskyi2022.exel_manipulation;
+
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import com.ykomarnytskyi2022.freight.Shipment;
+
+class ReadWriteMiddleman {
+	private Set<List<Shipment>> readyToWriteLists = new LinkedHashSet<>(); 
+	private Set<ExcelParser> fromSetOfFiles = new LinkedHashSet<>();
+	private ExcelWriter toFile;
+	private ExcelParser fromFile;
+	
+	public ReadWriteMiddleman(ExcelWriter ew, Set<ExcelParser> ep) {
+		toFile = ew;
+		fromSetOfFiles = ep;
+	}
+	public ReadWriteMiddleman(ExcelWriter ew, ExcelParser ep) {
+		toFile = ew;
+		fromFile = ep;
+	}
+	
+	public void readAndWrite() {
+		if(fromFile != null) {
+			List<Shipment> shipmList = ExcelWriter.mapFromFieldsTransToShipment(fromFile.parseFreightDataInSingleFile());
+			toFile.writeToExcel(ExcelWriter.pickRelevantToday(shipmList));
+		} else if (fromSetOfFiles.size() > 0) {
+			toFile.writeToExcelFromMultFiles(ExcelParser.readFromMultipleFiles(fromSetOfFiles));
+		}
+	}
+	
+	boolean offerExcelParser(ExcelParser ep) {
+		return fromSetOfFiles.add(ep);
+	}
+	
+	Stream<List<Shipment>> getReadyToWriteLists() {
+		return this.readyToWriteLists.stream();
+	}
+	
+	public static void main(String[] args) {
+		ExcelWriter write = new ExcelWriter(ExcelWriter.WRITE_TO, ExcelWriter.SHEET_NAME);
+		Set<ExcelParser> testSet = new LinkedHashSet<>();
+		testSet.add(new ExcelParser(write.STEEL_PATH, write.SEARCH_RESULTS));
+		testSet.add(new ExcelParser(write.CENTRIA_PATH, write.SEARCH_RESULTS));
+		testSet.add(new ExcelParser(write.MHS_PATH, write.SEARCH_RESULTS));
+		ReadWriteMiddleman tool = new ReadWriteMiddleman(write, testSet);
+		tool.readAndWrite();
+
+	}
+	
+}
