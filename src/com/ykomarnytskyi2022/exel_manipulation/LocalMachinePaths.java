e5d197597f9d2b8e132aec9c1be669e89ccb83bf
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2023-12-05 05:01:20 -0500

    Temporary class with static fields reflecting paths on local machine

---
+++ src/com/ykomarnytskyi2022/exel_manipulation/LocalMachinePaths.java
@@ -0,0 +1,11 @@
+package com.ykomarnytskyi2022.exel_manipulation;
+
+class LocalMachinePaths {
+	static String customerC = "C:\\Users\\LEMMY\\Desktop\\PlsTracking\\C.xls"; 
+	static String customerS = "C:\\Users\\LEMMY\\Desktop\\PlsTracking\\S.xls"; 
+	static String customerM = "C:\\Users\\LEMMY\\Desktop\\PlsTracking\\M.xls"; 
+
+	static final String SHEET_NAME = "Sheet1";
+	static final String SEARCH_RESULTS = "Search Results";
+	static String blankExelFilePath = "C:\\Users\\LEMMY\\Desktop\\PlsTracking\\FreightDemo.xls";
+}
