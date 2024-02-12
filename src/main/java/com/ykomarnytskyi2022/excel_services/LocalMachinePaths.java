49a35fd00b88327dc723bdacedabcf481c2220e6
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2024-02-12 13:23:43 -0500

    updated local system paths

--- src/main/java/com/ykomarnytskyi2022/excel_services/LocalMachinePaths.java
+++ src/main/java/com/ykomarnytskyi2022/excel_services/LocalMachinePaths.java
@@ -4,12 +4,18 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 class LocalMachinePaths {
-	
-	static Path customerC = Paths.get("C:\\Users\\LEMMY\\Desktop\\eclipse-workspace\\TrackAndTraceAssistant\\resources\\C.xls"); 
-	static Path customerS = Paths.get("C:\\Users\\LEMMY\\Desktop\\eclipse-workspace\\TrackAndTraceAssistant\\resources\\S.xls"); 
-	static Path customerM = Paths.get("C:\\Users\\LEMMY\\Desktop\\eclipse-workspace\\TrackAndTraceAssistant\\resources\\M.xls"); 
+
+	static Path customerC = Paths
+			.get("C:\\Users\\LEMMY\\Desktop\\eclipse-workspace\\TrackAndTraceAssistant\\src\\main\\resources\\C.xls");
+	static Path customerS = Paths
+			.get("C:\\Users\\LEMMY\\Desktop\\eclipse-workspace\\TrackAndTraceAssistant\\src\\main\\resources\\S.xls");
+	static Path customerM = Paths
+			.get("C:\\Users\\LEMMY\\Desktop\\eclipse-workspace\\TrackAndTraceAssistant\\src\\main\\resources\\M.xls");
+
+	static final String SHEET_NAME = "Sheet1";
+	static Path blankExelFile = Paths.get(
+			"C:\\Users\\LEMMY\\Desktop\\eclipse-workspace\\TrackAndTraceAssistant\\src\\main\\resources\\FreightDemo.xls");
 
 	static final String DEFAULT_SHEET_NAME = "Sheet1";
 	static final String SEARCH_RESULTS = "Search Results";
-	static Path blankExelFile = Paths.get("C:\\Users\\LEMMY\\Desktop\\eclipse-workspace\\TrackAndTraceAssistant\\resources\\FreightDemo.xls");
 }
