37b52cc6c48e1bb4507a7e10d0ccfdeb536c9996
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2024-02-13 01:58:18 -0500

    LocalMachinePaths modified
    * made it an abstract class to prevent any instantiations

--- src/main/java/com/ykomarnytskyi2022/excel_services/LocalMachinePaths.java
+++ src/main/java/com/ykomarnytskyi2022/excel_services/LocalMachinePaths.java
@@ -3,8 +3,7 @@ package com.ykomarnytskyi2022.excel_services;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-class LocalMachinePaths {
-
+abstract class LocalMachinePaths {
 	static Path customerC = Paths
 			.get("C:\\Users\\LEMMY\\Desktop\\eclipse-workspace\\TrackAndTraceAssistant\\src\\main\\resources\\C.xls");
 	static Path customerS = Paths
