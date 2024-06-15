a13bb064cbf642652df3079db5b381a4ffa9c8ca
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2024-06-15 21:18:06 +0200

    introducing base entity class to derive from

---
+++ src/main/java/com/ykomarnytskyi2022/domain/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.ykomarnytskyi2022.domain;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	protected Long getId() {
+		return id;
+	}
+
+	protected void setId(Long id) {
+		this.id = id;
+	}
+}
