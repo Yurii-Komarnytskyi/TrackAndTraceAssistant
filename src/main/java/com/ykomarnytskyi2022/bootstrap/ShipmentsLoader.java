c550217dcd95438bde2d3e7da3f99380e912dd13
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2024-07-12 15:53:57 +0200

    Updated imports (package names have been changed) New constructor using ShipmentRepository to save the shipments #13

--- src/main/java/com/ykomarnytskyi2022/bootstrap/ShipmentsLoader.java
+++ src/main/java/com/ykomarnytskyi2022/bootstrap/ShipmentsLoader.java
@@ -1,25 +1,33 @@
-package com.ykomarnytskyi2022.web.bootstrap;
+package com.ykomarnytskyi2022.bootstrap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import com.ykomarnytskyi2022.excel_services.ExcelOperations;
-import com.ykomarnytskyi2022.excel_services.ExcelOperationsImpl;
+import com.ykomarnytskyi2022.freight.Trackable;
+import com.ykomarnytskyi2022.repositories.ShipmentRepository;
+import com.ykomarnytskyi2022.services.excel.ExcelOperations;
 
 @Component
 public class ShipmentsLoader implements CommandLineRunner {
 	
-	final ExcelOperations delegator;
+	final ExcelOperations excelOperations;
+	final ShipmentRepository shipmentRepository;
 	
 	@Autowired
-	public ShipmentsLoader(ExcelOperations delegator) {
-		this.delegator = delegator;
+	public ShipmentsLoader(ExcelOperations excelOperations, ShipmentRepository shipmentRepository) {
+		this.excelOperations = excelOperations;
+		this.shipmentRepository = shipmentRepository;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		delegator.write(); 
+		excelOperations.write();
+		excelOperations.expose().stream().forEach(customerList -> {
+			customerList.stream().forEach(shipment -> {
+				shipmentRepository.save((Trackable)shipment);
+			});
+		});
 	}
 
 }
