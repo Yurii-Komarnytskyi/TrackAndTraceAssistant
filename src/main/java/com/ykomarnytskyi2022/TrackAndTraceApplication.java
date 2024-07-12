e4919069ebc2c95957f19f3c8c75c42b657ba53d
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2024-07-12 15:44:24 +0200

    removed context variable from main method

--- src/main/java/com/ykomarnytskyi2022/TrackAndTraceApplication.java
+++ src/main/java/com/ykomarnytskyi2022/TrackAndTraceApplication.java
@@ -2,11 +2,10 @@ package com.ykomarnytskyi2022;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class TrackAndTraceApplication {
 	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(TrackAndTraceApplication.class, args);
+		SpringApplication.run(TrackAndTraceApplication.class, args);
 	}
 }
