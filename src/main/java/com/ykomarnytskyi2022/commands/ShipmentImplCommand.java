2acb1a59b328bc313a2c1a8c4a19f875b5f37d9b
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2024-08-08 21:50:26 +0200

    Implemented editing & saving changes on shipments from client side closes #23

--- src/main/java/com/ykomarnytskyi2022/commands/ShipmentImplCommand.java
+++ src/main/java/com/ykomarnytskyi2022/commands/ShipmentImplCommand.java
@@ -12,21 +12,21 @@ import com.ykomarnytskyi2022.freight.Trackable;
 public class ShipmentImplCommand {
 
 	public Long id;
-	public String organizationName; 
-	public String shipmentID; 
-	public ShipmentStatus status; 
+	public String organizationName;
+	public String shipmentID;
+	public ShipmentStatus status;
 	public String scac;
-	public String originCity; 
+	public String originCity;
 	public String destinationCity;
 	public LocalDateTime PNET;
 	public LocalDateTime PNLT;
 	public LocalDateTime DNET;
 	public LocalDateTime DNLT;
-	
+
 	public ShipmentImplCommand(Shipment shipment) {
 		Trackable trackable = (Trackable) shipment;
 		Map<TimeFrameRequirements, LocalDateTime> map = shipment.getTimeFrameRequirements();
-		
+
 		id = trackable.getIdForCommandObj();
 		organizationName = trackable.getOrganizationName();
 		shipmentID = shipment.provideFieldsForExcelCells().get(0);
@@ -38,6 +38,64 @@ public class ShipmentImplCommand {
 		PNLT = map.get(TimeFrameRequirements.PICKUP_NOT_LATER_THAN);
 		DNET = map.get(TimeFrameRequirements.DELIVER_NOT_EARLIER_THAN);
 		PNET = map.get(TimeFrameRequirements.DELIVER_NOT_LATER_THAN);
+	}
+
+	public ShipmentImplCommand() {}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getOrganizationName() {
+		return organizationName;
+	}
+
+	public void setOrganizationName(String organizationName) {
+		this.organizationName = organizationName;
+	}
+
+	public String getShipmentID() {
+		return shipmentID;
+	}
+
+	public void setShipmentID(String shipmentID) {
+		this.shipmentID = shipmentID;
+	}
+
+	public ShipmentStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(ShipmentStatus status) {
+		this.status = status;
+	}
+
+	public String getScac() {
+		return scac;
+	}
+
+	public void setScac(String scac) {
+		this.scac = scac;
+	}
+
+	public String getOriginCity() {
+		return originCity;
+	}
+
+	public void setOriginCity(String originCity) {
+		this.originCity = originCity;
+	}
+
+	public String getDestinationCity() {
+		return destinationCity;
+	}
+
+	public void setDestinationCity(String destinationCity) {
+		this.destinationCity = destinationCity;
 	}
 
 	@Override
