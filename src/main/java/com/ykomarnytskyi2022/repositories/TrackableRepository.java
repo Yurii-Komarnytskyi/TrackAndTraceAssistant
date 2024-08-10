a81fcd0d8d8024cd99f007459be289203b2f1296
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2024-08-10 12:12:17 +0200

    Added NoRepositoryBean TrackableBaseRepository interface

--- src/main/java/com/ykomarnytskyi2022/repositories/TrackableRepository.java
+++ src/main/java/com/ykomarnytskyi2022/repositories/TrackableRepository.java
@@ -1,33 +1,11 @@
 package com.ykomarnytskyi2022.repositories;
 
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 
 import com.ykomarnytskyi2022.freight.Trackable;
 
-import jakarta.transaction.Transactional;
+public interface TrackableRepository extends ListCrudRepository<Trackable, Long>, TrackableBaseRepository {
 
-public interface TrackableRepository extends ListCrudRepository<Trackable, Long> {
 
-	@Transactional
-	@Modifying
-	@Query("update Trackable t set t.shipmentID = ?1 where t.id = ?2")
-	void setShipmentIDById(String shipmentID, Long id);
-
-	@Transactional
-	@Modifying
-	@Query("update Trackable t set t.scac = ?1 where t.id = ?2")
-	void setScacById(String scac, Long id);
-
-	@Transactional
-	@Modifying
-	@Query("update Trackable t set t.destinationCity = ?1 where t.id = ?2")
-	void setDestinationCityById(String destinationCity, Long id);
-
-	@Transactional
-	@Modifying
-	@Query("update Trackable t set t.originCity = ?1 where t.id = ?2")
-	void setOriginCityById(String originCity, Long id);
 
 }
